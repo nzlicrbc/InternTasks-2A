322d5f4275c5223d91e1e9b53b52800f866c7485
nzlicrbc <nzlcn10@hotmail.com>
2024-07-11 22:25:14 +0300

    made full detail screen

--- app/src/main/java/com/mobillium/interntasks2a/DetailActivity.kt
+++ app/src/main/java/com/mobillium/interntasks2a/DetailActivity.kt
@@ -1,4 +1,25 @@
 package com.mobillium.interntasks2a
 
-class DetailActivity {
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val weatherItem = intent.getParcelableExtra<WeatherItem>("weatherItem")
+
+        if (weatherItem != null) {
+
+            findViewById<TextView>(R.id.textViewTemperature).text = weatherItem.temperature
+            findViewById<TextView>(R.id.textViewTemperatureRange).text =
+                weatherItem.temperatureRange
+            findViewById<TextView>(R.id.textViewCityName).text = weatherItem.cityName
+            findViewById<TextView>(R.id.textViewWeatherDescription).text =
+                weatherItem.weatherDescription
+        }
+    }
 }
---
+++ app/src/main/java/com/mobillium/interntasks2a/WeatherItem.kt
@@ -0,0 +1,44 @@
+package com.mobillium.interntasks2a
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class WeatherItem(
+    var temperature: String,
+    val temperatureRange: String,
+    val cityName: String,
+    val weatherDescription: String,
+    val weatherCondition: String,
+    val itemId: Int = -1
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: ""
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(temperature)
+        parcel.writeString(temperatureRange)
+        parcel.writeString(cityName)
+        parcel.writeString(weatherDescription)
+        parcel.writeString(weatherCondition)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<WeatherItem> {
+        override fun createFromParcel(parcel: Parcel): WeatherItem {
+            return WeatherItem(parcel)
+        }
+
+        override fun newArray(size: Int): Array<WeatherItem?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
