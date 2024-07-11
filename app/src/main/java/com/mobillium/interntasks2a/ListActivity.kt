ffafa383615d52b4fd7162d49aefc775fddf4bff
nzlicrbc <nzlcn10@hotmail.com>
2024-07-11 22:31:55 +0300

    list activity screen done

--- app/src/main/java/com/mobillium/interntasks2a/ListActivity.kt
+++ app/src/main/java/com/mobillium/interntasks2a/ListActivity.kt
@@ -1,4 +1,26 @@
-package com.mobillium.interntasks2a
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mobillium.interntasks2a.R
+import com.mobillium.interntasks2a.WeatherItem
 
-class ListActivity {
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+
+        val weatherData = listOf(
+            WeatherItem("26°C", "14°C - 27°C", "İstanbul", "Güneşli", "sunny"),
+            WeatherItem("26°C", "14°C - 27°C", "Ankara", "Güneşli", "sunny"),
+            WeatherItem("26°C", "14°C - 27°C", "Erzurum", "Güneşli", "sunny"),
+            WeatherItem("26°C", "14°C - 27°C", "Sakarya", "Güneşli", "sunny"),
+        )
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerViewWeather)
+        recyclerView.adapter = WeatherAdapter(weatherData, this) { weatherItem ->
+        }
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
 }
