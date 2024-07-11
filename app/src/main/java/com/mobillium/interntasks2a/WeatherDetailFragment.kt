86ff6bd01f83280ee9857ad5e1f2d87ca81bf458
nzlicrbc <nzlcn10@hotmail.com>
2024-07-11 22:28:04 +0300

    navigation done

---
+++ app/src/main/java/com/mobillium/interntasks2a/WeatherDetailFragment.kt
@@ -0,0 +1,63 @@
+package com.mobillium.interntasks2a
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import kotlin.random.Random
+
+class WeatherDetailFragment : Fragment() {
+
+    private lateinit var temperatureTextView: TextView
+    private var currentTemperature: String = ""
+    private var itemId: Int = -1
+    private val args: WeatherDetailFragmentArgs by navArgs()
+    private val viewModel: WeatherViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.activity_detail, container, false)
+
+        temperatureTextView = view.findViewById(R.id.textViewTemperature)
+        val refreshIcon = view.findViewById<ImageView>(R.id.imageViewRefreshIcon)
+        val updateButton = view.findViewById<Button>(R.id.buttonUpdateData)
+
+        val weatherItem = args.weatherItem
+
+        if (weatherItem != null) {
+            currentTemperature = weatherItem.temperature
+            itemId = weatherItem.itemId
+            temperatureTextView.text = currentTemperature
+        }
+
+        refreshIcon.setOnClickListener {
+            generateRandomTemperature()
+
+            viewModel.updateTemperature(itemId, currentTemperature)
+        }
+
+        updateButton.setOnClickListener {
+            viewModel.updateTemperature(itemId, currentTemperature)
+            findNavController().popBackStack()
+        }
+
+        return view
+    }
+
+    private fun generateRandomTemperature() {
+        val minTemp = 14
+        val maxTemp = 27
+        val randomTemperature = Random.nextInt(minTemp, maxTemp + 1)
+        currentTemperature = "$randomTemperature°C"
+        temperatureTextView.text = currentTemperature
+    }
+}
---
+++ app/src/main/java/com/mobillium/interntasks2a/WeatherListFragment.kt
@@ -0,0 +1,68 @@
+package com.mobillium.interntasks2a
+
+import WeatherAdapter
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class WeatherListFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var weatherAdapter: WeatherAdapter
+    private val viewModel: WeatherViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.activity_list, container, false)
+
+        recyclerView = view.findViewById(R.id.recyclerViewWeather)
+        weatherAdapter = WeatherAdapter(
+            viewModel.weatherItems.value ?: emptyList(),
+            requireContext()
+        ) { weatherItem ->
+            val action =
+                WeatherListFragmentDirections.actionWeatherListFragmentToWeatherDetailFragment(
+                    weatherItem
+                )
+            findNavController().navigate(action)
+        }
+
+        recyclerView.adapter = weatherAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.weatherItems.observe(viewLifecycleOwner) { updatedWeatherItems ->
+            if (updatedWeatherItems != null) {
+                weatherAdapter.updateData(updatedWeatherItems)
+            }
+        }
+
+        if (viewModel.weatherItems.value.isNullOrEmpty()) {
+            viewModel.weatherItems.value = getWeatherData()
+        }
+    }
+
+
+    private fun getWeatherData(): List<WeatherItem> {
+        var itemIdCounter = 0
+        return listOf(
+            WeatherItem("26°C", "14°C - 27°C", "İstanbul", "Güneşli", "sunny", itemIdCounter++),
+            WeatherItem("26°C", "14°C - 27°C", "Ankara", "Güneşli", "sunny", itemIdCounter++),
+            WeatherItem("26°C", "14°C - 27°C", "Erzurum", "Güneşli", "sunny", itemIdCounter++),
+            WeatherItem("26°C", "14°C - 27°C", "Sakarya", "Güneşli", "sunny", itemIdCounter++),
+        )
+    }
+}
